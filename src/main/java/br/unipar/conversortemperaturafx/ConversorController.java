21dec635cde22ccdb88e4e77c000f087fb658f55
Feyre <inaraoenning@gmail.com>
2025-11-17 21:23:59 -0300

    Add

--- src/main/java/br/unipar/conversortemperaturafx/ConversorController.java
+++ src/main/java/br/unipar/conversortemperaturafx/ConversorController.java
@@ -3,7 +3,7 @@ package br.unipar.conversortemperaturafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ConversorController {
     @FXML
     private Label welcomeText;
 
